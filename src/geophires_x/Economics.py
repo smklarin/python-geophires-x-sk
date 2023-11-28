ae0724fc71a64c429214d5dd8dc55b23e710214e
Sam Klarin <sam.klarin@gmail.com>
2023-11-28 15:13:52 -0600

    Changed AC correlation to 1500/ton, 5% contingency, 0% indirect cost

--- src/geophires_x/Economics.py
+++ src/geophires_x/Economics.py
@@ -1892,8 +1892,8 @@ class Economics:
                 self.Cplant.value = 1.0 * 1.05 * self.ccplantadjfactor.value * 250E-6 * np.max(
                     model.surfaceplant.HeatExtracted.value) * 1000.  # 1.15 for 15% contingency and 1.12 for 12% indirect costs, changed to 5% contingency and 0% indirect (SK)
                 if self.chillercapex.value == -1:  # no value provided by user, use built-in correlation ($2500/ton)
-                    self.chillercapex.value = 1.12 * 1.15 * np.max(
-                        model.surfaceplant.CoolingProduced.value) * 1000 / 3.517 * 1500 / 1e6  # $1,500/ton of cooling(SK). 1.15 for 15% contingency and 1.12 for 12% indirect costs
+                    self.chillercapex.value = 1.0 * 1.05 * np.max(
+                        model.surfaceplant.CoolingProduced.value) * 1000 / 3.517 * 1500 / 1e6  # $1,500/ton of cooling(SK). 1.15 for 15% contingency and 1.12 for 12% indirect costs, changed to 5% contingency and 0% indirect (SK)
 
                 # now add chiller cost to surface plant cost
                 self.Cplant.value += self.chillercapex.value
